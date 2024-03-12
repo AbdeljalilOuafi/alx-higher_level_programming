e8327f9b1d38c171e5f5732dbd45feb84bafc1fa
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-03-12 22:01:44 +0000

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    string = ""
+    for char in str:
+        if ord(char) >= ord('a') and ord(char) <= ord('z'):
+            string += chr(ord(char) - 32)
+        else:
+            string += char
+    print(string)
+
+
