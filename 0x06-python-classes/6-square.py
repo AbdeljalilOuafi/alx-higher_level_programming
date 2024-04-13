ad3ae13155a05583c8f69a8c625435a1cf2a0cb0
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-04-13 19:21:37 +0000

    fix: Remove Unnecessary code and improve readability

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
 
-"""Define a Square Class"""
+""" define a class Square"""
 
 
-class Square:
-    """The Square Class"""
+class Square():
+    """ the class square """
     def __init__(self, size=0, position=(0, 0)):
-        if not isinstance(position, tuple) or len(position) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif not all(isinstance(num, int) and num >= 0 for num in position):
-            raise ValueError("position elements must be positive integers")
+        """Initialize a new square.
 
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-            self.__position = position
+        Args:
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
+        """
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
         return self.__size
-
-    @property
-    def position(self):
-        return self.__position
 
     @size.setter
     def size(self, value):
@@ -36,24 +28,29 @@ class Square:
         else:
             self.__size = value
 
+    @property
+    def position(self):
+        return self.__position
+
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2:
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not all(isinstance(num, int) and num >= 0 for num in value):
-            raise ValueError("position elements must be positive integers")
-        else:
-            self.__position = value
+        self.__position = value
 
     def area(self):
-        return self.__size ** 2
+        return (self.__size * self.__size)
 
     def my_print(self):
-        if self.size == 0:
+        if self.__size == 0:
             print()
             return
         [print() for i in range(0, self.__position[1])]
         for i in range(self.__size):
             print(" " * self.__position[0], end="")
-            for j in range(self.size):
-                print("#", end="" if j < self.size - 1 else "\n")
+            for j in range(self.__size):
+                print("#", end="")
+            print()
