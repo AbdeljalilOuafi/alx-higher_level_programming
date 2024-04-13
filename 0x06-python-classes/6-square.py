7580804fe00dbd7785670ac1b939718195c75aee
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-04-13 19:01:13 +0000

    refactor: position.setter

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -36,10 +36,11 @@ class Square:
     def position(self, value):
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif not all(isinstance(num, int) and num >= 0 for num in value):
+        if not all(isinstance(num, int) and num >= 0 for num in value):
             raise ValueError("position elements must be positive integers")
         else:
             self.__position = value
+
     def area(self):
         return self.__size ** 2
 
