ddbca828eb7fa9d7d016f3083edb3ab5a01aa9b0
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-04-27 01:14:05 +0100

    refactor: __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -90,8 +90,10 @@ class Rectangle(Base):
 
     def __str__(self) -> str:
         """returns the string"""
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - \
-            {self.__width}/{self.__height}"
+        return (
+            f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - "
+            f"{self.__width}/{self.__height}"
+        )
 
     def update(self, *args, **kwargs):
         """Update the Rectangle.
