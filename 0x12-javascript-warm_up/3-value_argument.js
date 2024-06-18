00f47b0a3d76705915ea1096661a93e175fed05f
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-06-18 22:48:16 +0100

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const args = process.argv.slice(2);
+let lengthOfArgs = 0;
+for (let i = 0; i < args.length; i++) {
+  lengthOfArgs += 1;
+}
+
+if (!lengthOfArgs) {
+  console.log('No argument');
+} else {
+  console.log(args[0]);
+}
