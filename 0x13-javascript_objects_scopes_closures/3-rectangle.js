87d656cb75dd7abfe239ea60620b69317605c620
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-06-19 19:40:13 +0100

    4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -17,5 +17,16 @@ class Rectangle {
       console.log(string);
     }
   }
+  
+  rotate () {
+    const swap = this.height;
+    this.height = this.width;
+    this.width = swap;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
 }
 module.exports = Rectangle;
