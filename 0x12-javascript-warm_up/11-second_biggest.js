1f6c178ca0334b2324705fee6114fe02608c8525
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-06-19 05:47:57 +0100

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const argv = process.argv.slice(2);
+if (argv.length === 0) {
+  console.log(0);
+} else if (argv.length === 1) {
+  console.log(1);
+} else {
+  argv.sort((a, b) => b - a);
+  console.log(argv[1])
+}
