0ca4d85fedf4698503f1b2647b94a43a71a2dc17
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-06-21 20:33:56 +0100

    add_semicolon_to_query

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,11 +3,11 @@
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
-    
+
     db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
 
-    sql_query = "SELECT * FROM states ORDER BY id"
+    sql_query = "SELECT * FROM states ORDER BY id;"
     cursor.execute(sql_query)
     result = cursor.fetchall()
 
