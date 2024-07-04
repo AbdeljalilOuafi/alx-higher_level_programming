6eb5dc3a2f893f2fbd68ee421dc869c7ae6e0f89
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-07-04 13:36:20 +0100

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,7 +2,9 @@
 #this script takes in a URL, sends a request to that URL,
 #and displays the size of the body of the response
 
-url=$1
-size=$(curl -s "$url" | wc -c)
-echo "$size"
+#-s: run curl in silent mode
+#-I: bring the header only, not the whole content
+#$@: all arguments, in this case its going to be the url to be fetched
+
+curl -sI $@ | grep 'Content-Length:' | cut -d ' ' -f2
 
