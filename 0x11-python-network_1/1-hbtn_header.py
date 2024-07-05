35ac2034722a77b324bae5e651d46a6e71cf0c0b
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-07-05 15:06:19 +0100

    1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+from urllib import request
+from sys import argv
+
+url = argv[1]
+
+with request.urlopen(url) as response:
+    data = response.headers
+    print(data["X-Request-Id"])
