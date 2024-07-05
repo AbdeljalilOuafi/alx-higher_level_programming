23a47aebfa9c4869794fa0ad674a3e7db5b7115d
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-07-05 17:17:38 +0100

    6-post_email.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"""5-hbtn_header module"""
-import requests
-from sys import argv
 
-if __name__ == "__main__":
     response = requests.get(argv[1])
     print(response.headers.get("X-Request-Id"))
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""5-hbtn_header module"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.post(argv[1], data={'email': argv[2]})
+    print(response.text)
