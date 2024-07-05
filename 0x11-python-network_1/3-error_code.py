757690ae36bf07e551ce591ec7447bbf1ac26537
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-07-05 16:36:52 +0100

    fix-output

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,6 +9,4 @@ if __name__ == "__main__":
             print(result.decode("utf-8"))
 
     except error.HTTPError as e:
-        print("Error code:{}".format(e.code))
-
-
+        print("Error code: {}".format(e.code))
