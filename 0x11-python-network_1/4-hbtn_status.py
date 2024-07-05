f3403506f8e0eb84737b398eafe6a35ae0de02fd
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-07-05 16:59:19 +0100

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,6 +4,7 @@ import requests
 
 if __name__ == "__main__":
     response = requests.get("https://alx-intranet.hbtn.io/status")
-
-    print("Body response:\n\t- type: {}\n\
-    \t- content: {}".format(type(response.text), response.text))
+    print("Body response:\n\t\
+    - type: {}\n\t\
+    - content: {}\n\t".format(
+        type(response), response.text))
