5921ff21a677dcbd923e154436624d24f3bc589b
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-07-05 16:33:57 +0100

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""error_code module"""
+from urllib import request, error
+from sys import argv
+
+try:
+    with request.urlopen(argv[1]) as response:
+        result = response.read()
+        print(result.decode("utf-8"))
+
+except error.HTTPError as e:
+    print("Error code:{}".format(e.code))
+
+
