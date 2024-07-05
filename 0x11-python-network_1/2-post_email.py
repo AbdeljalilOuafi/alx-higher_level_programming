a0dff8076dd0358fe473b110b7568b3202717451
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-07-05 15:51:03 +0100

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+from urllib import request, parse
+from sys import argv
+
+data = parse.urlencode({'email': argv[2]}).encode('utf-8')
+req = request.Request(argv[1], data)
+
+with request.urlopen(req) as response:
+    response_data = response.read()  # Read the response
+
+print(response_data.decode('utf-8'))
